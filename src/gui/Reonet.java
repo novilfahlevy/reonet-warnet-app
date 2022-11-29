46bdc1a74739fec3bd59853fa2f844b9a796546d
novilfahlevy <novilfreon@gmail.com>
2022-11-29 22:03:51 +0800

    login frame gui

---
+++ src/gui/Reonet.java
@@ -0,0 +1,11 @@
+package gui;
+
+import gui.Login;
+
+public class Reonet {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
